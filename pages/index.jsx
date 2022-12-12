7fe12075a281824235891040479fd4a4e08509d5
Eduard Dyckman <bird.duskpoet@gmail.com>
2022-12-12 17:33:43 +0200

    Bump 1

--- pages/index.jsx
+++ pages/index.jsx
@@ -67,7 +67,7 @@ export default function Home({ intialTodos }) {
   return (
     <>
       <Head>
-        <title>Todo App Ver 12</title>
+        <title>Todo App Ver 13</title>
         <link rel='icon' href='https://neon.tech/favicon-32x32.png' />
       </Head>
 
