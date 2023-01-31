c6d1c719f637a5a75ede097153fc29c34ddecc89
Eduard Dyckman <bird.duskpoet@gmail.com>
2023-01-31 17:21:17 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -67,7 +67,7 @@ export default function Home({ intialTodos }) {
   return (
     <>
       <Head>
-        <title>Todo App PREVIEW v94</title>
+        <title>Todo App PREVIEW v95</title>
         <link rel='icon' href='https://neon.tech/favicon-32x32.png' />
       </Head>
 
